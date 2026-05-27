d5bfb404ac719e4aba77395e5ce3cab8e6c6f166
rokan45 <rokanuzzaman4567@gmail.com>
2026-05-27 17:24:08 +0300

    added not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>This page is not currently available</h1>
+        <div className='min-h-screen text-center items-center justify-center flex flex-col'>
+            <h1 className='font-bold text-red-400 text-3xl mb-5'>This page is not currently available</h1>
+
+            <Link href={'/'}>
+            <button className='btn btn-active'>Back To Homepage</button>
+            </Link>
         </div>
     );
 };
